991ee1bcf49edfd60a3cac49fe361cab54008b79
MahmoudAlaa92 <mahmoudalaa.wr@gmail.com>
2024-12-18 01:14:40 +0200

    Add Unit Test

---
+++ EnhancementTests/EnhancementTests.swift
@@ -0,0 +1,32 @@
+//
+//  EnhancementTests.swift
+//  EnhancementTests
+//
+//  Created by Mahmoud Alaa on 18/12/2024.
+//
+
+import XCTest
+@testable import Enhancement
+
+final class EnhancementTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        print("setUP")
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        print("tearDown")
+    }
+    
+    func test() throws {
+        print("Test 1")
+
+    }
+    
+    func test2() throws {
+        print("Test 2")
+    }
+
+}
