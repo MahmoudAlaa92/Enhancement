039291b611fb392f45f1f06e9b1be3b3aeaf98f1
MahmoudAlaa92 <mahmoudalaa.wr@gmail.com>
2025-04-09 15:56:43 +0200

    implement PassThroughSubject 🛳️

--- Enhancement/SceneDelegate.swift
+++ Enhancement/SceneDelegate.swift
@@ -11,12 +11,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
       
         guard let sceneWindow = (scene as? UIWindowScene) else { return }
         window =  UIWindow(windowScene: sceneWindow)
-        window?.rootViewController = TestViewController()
+        window?.rootViewController = PassThroughSubjectViewController()
         window?.makeKeyAndVisible()
     }
 
